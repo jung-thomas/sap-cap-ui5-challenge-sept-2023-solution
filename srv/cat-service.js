2164d0a799fd488bc67b47d9a0a0572df55d6341
Thomas Jung <thomas.jung@sap.com>
2023-09-01 17:51:19 -0400

    Question Validation

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -7,12 +7,23 @@ module.exports = class DevChallengeService extends cds.ApplicationService {
     }
 
     async generateTestQuestions(TestsIn, keys, questionsCount) {
-        console.log(questionsCount)
         const { Tests, Questions } = this.entities()
+        let message = ``
+        if (!questionsCount || questionsCount < 1){
+            message = `You asked for zero questions. Nothing to do.`
+            return {"message": message}
+        }
+
         const expr = cds.parse.expr(`test_ID is null`)
         const Items = await cds.run(
             SELECT.from(Questions).where(expr).limit(questionsCount))
-      
+
+        if(!Items || Items.length === 0){
+            message =  `No Questions remaining that can be assigned to a test. Please create more`
+            return {"message": message}
+        }
+
+
         Items.forEach(item => {
             item.test_ID = keys.ID
             const expr = cds.parse.expr(`ID = '${item.ID}'`)
@@ -22,7 +33,12 @@ module.exports = class DevChallengeService extends cds.ApplicationService {
         const TestOutput = await cds.run(            
             SELECT.from(Tests).where(expr2)
         )
-        return { "message": `${questionsCount} questions successfully added to the test!`,
+        if (questionsCount > Items.length){
+            message = `Only ${Items.length} questions available, but you requested ${questionsCount}`
+        }else {
+            message = `${questionsCount} questions successfully added to the test!`
+        }
+        return { "message": message,
           "randomQuestions": Items, "testQuestions": TestOutput }
     }
 
